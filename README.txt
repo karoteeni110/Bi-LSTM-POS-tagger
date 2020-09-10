1ae308ad143b425fa4a327259232c2f39abef86e
AA <30997769+karoteeni110@users.noreply.github.com>
2020-09-10 22:47:40 +0300

    init

---
+++ README.txt
@@ -0,0 +1,10 @@
+Usage: 
+
+- For model with auxilary loss: 
+	python3 auxloss_model.py
+- For model with NO auxilary loss:
+	python3 no_auxlooss_model.py
+- For the most-frequent-baseline model:
+	python3 most-freq_baseline.py
+- To change over languages, modify the paths for reading datasets:
+	Change `TRAIN_FPATH`, `TEST_FPATH`, `DEV_FPATH` in data_ud.py
---
+++ src/data_ud.py
@@ -0,0 +1,136 @@
+"""https://universaldependencies.org/treebanks/fi_tdt/index.html"""
+import csv, sys, math
+from os import listdir
+from os.path import join
+from collections import Counter, defaultdict
+from path import data_path
+
+# TRAIN_FPATH =  join(data_path,'fi_tdt-ud-train.conllu.txt')
+# TEST_FPATH =  join(data_path,'fi_tdt-ud-test.conllu.txt')
+# DEV_FPATH =  join(data_path,'fi_tdt-ud-dev.conllu.txt')
+
+TRAIN_FPATH = join(data_path, 'sv_talbanken-ud-train.conllu.txt')
+TEST_FPATH =  join(data_path,'sv_talbanken-ud-test.conllu.txt')
+DEV_FPATH =  join(data_path,'sv_talbanken-ud-dev.conllu.txt')
+
+# TRAIN_FPATH =  join(data_path,'de_hdt-ud-train.conllu.txt')
+# TEST_FPATH =  join(data_path,'de_hdt-ud-test.conllu.txt')
+# DEV_FPATH =  join(data_path,'de_hdt-ud-train.conllu.txt')
+
+print('TRAIN:', TRAIN_FPATH)
+print('TEST:', TEST_FPATH)
+print('DEV:', DEV_FPATH)
+
+def logfreq(freq):
+    return int(math.log(freq))
+
+def get_byte2ix(training_data):
+    byte_to_ix = {'#UNK#':0}
+    for sent, _ in training_data:
+        for word in sent:
+            for c in word:
+                for bt in list(c.encode()):
+                    if bt not in byte_to_ix:
+                        byte_to_ix[bt] = len(byte_to_ix)
+    return byte_to_ix 
+
+def get_char2ix(training_data):
+    char_to_ix = {'#UNK#':0}
+    for sent, _ in training_data:
+        for word in sent:
+            for char in word:
+                if char not in char_to_ix:
+                    char_to_ix[char] = len(char_to_ix)
+    return char_to_ix
+
+def get_word2ix(training_data):
+    word_to_ix = {'#UNK#':0}
+    for sent, _ in training_data:
+        for word in sent:
+            if word not in word_to_ix:
+                word_to_ix[word] = len(word_to_ix)
+    return word_to_ix
+
+def get_tag2ix(training_data):
+    tag_to_ix = {'#UNK#':0}
+    for _, tags in training_data:
+        for t in tags:
+            if t not in tag_to_ix:
+                tag_to_ix[t] = len(tag_to_ix)
+    return tag_to_ix
+
+def get_freq2ix(training_data):
+    freq_to_ix = {'#UNK#':0}
+    all_tokens = []
+    for sent, _ in training_data:
+        all_tokens.extend(sent)
+    token_to_logfreq = Counter(all_tokens) 
+    for t in token_to_logfreq:
+        token_to_logfreq[t] = logfreq(token_to_logfreq[t])
+    for freq in token_to_logfreq.values():
+        if freq not in freq_to_ix: 
+            freq_to_ix[freq] = len(freq_to_ix)
+    return freq_to_ix, token_to_logfreq
+
+def get_tagfreq2ix(training_data):
+    """Should not be used"""
+    freq_to_ix = {'#UNK#':0}
+    all_tags = []
+    for _, tags in training_data:
+        all_tags.extend(tags)
+    tag_to_logfreq = Counter(all_tags) 
+    # Log of the freqs
+    for t in tag_to_logfreq:
+        tag_to_logfreq[t] = logfreq(tag_to_logfreq[t])
+    for freq in tag_to_logfreq.values():
+        if freq not in freq_to_ix: 
+            freq_to_ix[freq] = len(freq_to_ix)
+    return freq_to_ix, tag_to_logfreq
+
+def read_conllu(filename):
+    """
+    Read a list of sentences with POS labels from @sefilename. Each
+    sentence is a tuple of elements:
+    
+    BODY      - List of tokens of the sentence.
+    POS       - POS label for the sentence.
+
+    """
+    data = []
+
+    with open(filename, encoding='utf-8') as sefile:
+        token_stack, tag_stack = [], []
+        add_to_stack = False
+        for line in sefile.readlines():
+            rand_line = line.split()
+            if rand_line == []:
+                add_to_stack = False
+                data.append((token_stack,tag_stack))
+                token_stack, tag_stack = [], []
+                continue
+            if rand_line[0] == '1':
+                add_to_stack = True
+
+            if add_to_stack:
+                token_stack.append(rand_line[1].lower())
+                tag_stack.append(rand_line[3])
+    return data
+
+# training_data = [
+#     ("The dog ate the apple".split(), ["DET", "NN", "V", "DET", "NN"]),
+#     ("Everybody read that book".split(), ["NN", "V", "DET", "NN"])
+# ]
+training_data = read_conllu(TRAIN_FPATH)
+test_data = read_conllu(TEST_FPATH)
+dev_data = read_conllu(DEV_FPATH)
+
+word_to_ix = get_word2ix(training_data)
+byte_to_ix, char_to_ix = get_byte2ix(training_data), get_char2ix(training_data)
+tag_to_ix = get_tag2ix(training_data) # {"DET": 0, "NN": 1, "V": 2}
+freq_to_ix, token_to_freq = get_freq2ix(training_data) #{"DET": logfreq(3), "NN": logfreq(4), "V": logfreq(2)}
+
+if __name__=="__main__":
+    # Check that we don't crash on reading.
+    _, tagcounter = get_tagfreq2ix(training_data)
+    print(token_to_freq.most_common()[:10])
+
